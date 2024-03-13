3508f7130a2f2354777d2e631c9fa795bb1158d8
Aleksei Roznov <lyoza@Alekseis-MacBook-Pro.local>
2024-03-13 11:41:34 +0100

    update type file

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -1,16 +1,16 @@
-import { ICalendar } from "../interfaces/interfaces";
+import { CalendarType } from "../utils/types";
 import { ICalendarService } from "./calendar.interface";
 
 export class CalendarService implements ICalendarService {
     constructor() {}
 
-    async getCalendarData (): Promise<ICalendar>{
+    async getCalendarData (): Promise<CalendarType>{
         if (!process.env.CALEND) {
             console.error("Missing required environmental variables for Calendar");
         }
         let apiCall = await fetch(process.env.CALEND as string);
         let apiResponse = await apiCall.json();
         // retry if error
-        return apiResponse as ICalendar;
+        return apiResponse as CalendarType;
     };
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -28,3 +28,15 @@ export type UserType = {
     };
     rating: number;
 }
+
+export type CalendarEventType = {
+    start: {
+        date: string;
+    };
+    summary: string;
+    description?: string;
+}
+
+export type CalendarType = {
+    items: CalendarEventType[];
+}
